2e37ebbfa116703d4a34a8591b6c45536ba127cf
Jas1singh <jaswantsingh80209@gmail.com>
2026-07-06 02:23:57 +0530

    commit

--- Python/Assignment 15/Q9.py
+++ Python/Assignment 15/Q9.py
@@ -8,9 +8,9 @@ for i in range(1,n+1):
     for j in range(1,n-i+1):
         print(" ",end="")
     
-    for k in range(0,i):
+    for k in range(1,i+1):
         if k % 2==0:
-            print(k,end=" ")
+            print("0",end="")
 
         else:
-            print(k+1,end=" ")    
+            print("1",end="")    
